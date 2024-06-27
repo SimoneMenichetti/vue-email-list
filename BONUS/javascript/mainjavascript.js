73ea7e5997c6683593a4ee4e6b79691d0762dcf1
simone menichetti <simon3menichettui95@gmail.com>
2024-06-27 16:20:43 +0200

    feat:(Using promise.all to tell the array to resolve everything that needs to be resolved in the array/ feat:(html add a v-if to give the condition if v-if has tot elements show)  feat:(using mounted by invoking the apiEmails function as soon as the components are mounted

--- BONUS/javascript/mainjavascript.js
+++ BONUS/javascript/mainjavascript.js
@@ -10,23 +10,24 @@ createApp({
     methods:{
 
         apiEmails() {
-            // creazione di un nuovo array per le email recuperate prima del caricamento
-            const promiseEmails = [],
+            // creazione di un nuovo array per le email recuperate prima del caricamento e le nuove promesse da risolvere singolarmente
+            const promiseEmails = [];
             // mandiamo 10 richieste all api con un ciclo for 
             for (let i = 0; i < 10; i++) {
                 // utilizziamo la proprietà push per inserire nel array di email promesse le mail recuperate 
                 promiseEmails.push(axios.get('https://flynn.boolean.careers/exercises/api/random/mail'));
             }
-
-                    .then((risposta) => {
-                        // insieriamo le mail generate e recuperate da axios nel nostro array emails
-                        this.emails.push(risposta.data.response);
-                    })
-
-                    // inserimento proprietà catch per catturare e rilevare un possibile errore di recupero dal api
-                    .catch((error) => {
-                        console.error('Errore recupero email:', error);
-                    });
+            
+            // utilizzo di promise.all per riferire all array di risolvere tutto cio che va risolto nell array
+            Promise.all(promiseEmails)
+            .then((responses) => {
+                this.emails = responses.map(response => response.data.response);
+                console.log('Emails :', this.emails);
+            })
+                // inserimento proprietà catch per catturare e rilevare un possibile errore di recupero dal api
+            .catch((error) => {
+                console.error('Errore recupero email:', error);
+            });
            
         },
     },
